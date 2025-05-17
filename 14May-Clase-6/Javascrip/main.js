f2fdd5d923fb6b8fc5c2d1277a83b93d238a8934
hgonzalomarquez <hgonzalomarquez@gmail.com>
2025-05-17 02:22:27 +0000

    javascript

---
+++ 14May-Clase-6/Javascrip/main.js
@@ -0,0 +1,38 @@
+let nombre  = "Angelo";     // 
+let id      = 101479910;    //variale   tipo numerico
+
+
+
+const anio_nacimiento= 2000;  //constante tipo numerico
+
+let cadena = "Subiendo montañas en el atardecer";   // Variable string
+let numero = 56;                // Variable tipo número
+let decimal = 10.4567;          // Variable tipo decimal
+
+let booleano = true;            // Variable tipo booleano
+let indefinido = undefined;     // Variable tipo indefinido
+let nulo = null;                // Variable tipo nulo
+
+    console.log(typeof cadena);
+    console.log(typeof numero);
+    console.log(typeof decimal);
+    console.log(typeof booleano);
+    console.log(typeof indefinido);
+    console.log(typeof nulo);
+
+    //Operadores matematicos
+
+    let a = 2;
+    let b = 10;
+    let c = 25;
+    
+    console.log(a + b);  //Suma = 12
+    console.log(b - a);  //Resta = 8
+    console.log(a * b);  //Multi = 20;
+    console.log(b / a);  //Divi = 5
+    console.log(b % a);  //Modulo = 0
+
+    
+    console.log( a ** 2); //potencia
+    console.log("la raiz cuadrada de 25 es: 5")
+    console.log( c ** 0.5);
