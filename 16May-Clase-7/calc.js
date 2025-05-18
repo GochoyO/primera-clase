e8c71646609bfc5fddb96b47e5e3a9f9e276ed4b
hgonzalomarquez <hgonzalomarquez@gmail.com>
2025-05-18 23:47:41 +0000

    Se agrego Raiz cuadrada

--- 16May-Clase-7/calc.js
+++ 16May-Clase-7/calc.js
@@ -58,3 +58,21 @@ const divide = () => {
     document.getElementById("salida").textContent = "Resultado de la división es: " + resultado;
     console.log("!Felicidades¡... La división finalizó exitosamente")
 }
+
+function raiz(){
+    let n1 = Number(document.getElementsByName("Numero 1")[0].value);
+    let n2 = Number(document.getElementsByName("Numero 2")[0].value);
+
+    console.log("El numero 1 es", n1);
+    console.log("El numero 2 es", n2);
+
+    let resultado = Math.sqrt(n1);
+    let resultado1 = Math.sqrt(n2);
+    
+    
+    console.log("El resultado es", resultado);
+
+    document.getElementById("salida").textContent = "Resultado: la raiz cuadrada de: "+ n1  + " es: " + resultado 
+    + " y de: "+ n2  + " es: " + resultado1;
+    console.log("!Felicidades¡... La raiz cuadrada finalizó exitosamente")
+}
