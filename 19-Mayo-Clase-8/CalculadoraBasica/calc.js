aec8fb50efe25eeb9238d38666261bba49d83314
hgonzalomarquez <hgonzalomarquez@gmail.com>
2025-05-22 17:02:10 +0000

    Publicando Calculadora Personal

--- 19-Mayo-Clase-8/CalculadoraBasica/calc.js
+++ 19-Mayo-Clase-8/CalculadoraBasica/calc.js
@@ -1,27 +1,32 @@
 
-const botonAll = document.querySelectorAll(".bot-1");
+const botonAll = document.querySelectorAll("#bot-1");
 
-let operacion = "";
+let operacion = String("Blanco");
 
 botonAll.forEach(button=> {
     button.addEventListener("click", () => {
 
+     operacion = String(button.textContent);
 
-    operacion = document.getElementById(botonAll).textContent;
-
-    console.log("Se hizo clic en el botón:", document.getElementById(botonAll).textContent);
-
-    if(operacion = "Suma"){
-        suma;
-    }else if(operacion = "Resta"){
-        resta;
-    }else if(operacion = "Multiplicaciòn"){
-        multiplica;
-    }else if(operacion = "División"){
-        divide;
-         }else{
-        raiz;   
+    console.log("Se hizo clic en el botón - Fuera if:", operacion);
+    
+    if(operacion == "Resta"){
+        console.log("Se hizo clic en el botón  resta - Paso");
+        resta();
+    }else if(operacion =="Suma"){
+        console.log("Se hizo clic en el botón suma- Paso");
+        suma();
+    }else if(operacion == "Multiplicación"){
+        console.log("Se hizo clic en el botón Multiplicación- Paso");
+        multiplica();
+    }else if(operacion == "División"){
+        console.log("Se hizo clic en el botón División- Paso");
+        divide();
+    }else{
+            console.log("Se hizo clic en el botón raiz cuadrada - Paso");
+        raiz();   
          };
+
     });
 });
 
@@ -47,8 +52,8 @@ const resta = () => {
     let n1 = Number(document.getElementsByName("Numero 1")[0].value);
     let n2 = Number(document.getElementsByName("Numero 2")[0].value);
     
-    console.log("El numero 1 es", n1);
-    console.log("El numero 2 es", n2);
+    console.log("El numero 1 de la Resta es", n1);
+    console.log("El numero 2 dela Resta es", n2);
 
     let resultado = n1 - n2;
     console.log("El resultado es", resultado);
@@ -82,6 +87,7 @@ const divide = () => {
     console.log("El numero 2 es", n2);
 
     let resultado = n1 / n2;
+
     console.log("El resultado es", resultado);
 
     document.getElementById("salida").textContent = "Resultado de la división es: " + resultado;
@@ -95,14 +101,14 @@ function raiz(){
     console.log("El numero 1 es", n1);
     console.log("El numero 2 es", n2);
 
-    let resultado = Math.sqrt(n1);
-    let resultado1 = Math.sqrt(n2);
+    let resultadoR1 = Math.sqrt(n1);
+    let resultadoR2 = Math.sqrt(n2);
     
     
-    console.log("El resultado es", resultado);
+    console.log("El resultado del número 1, es :", resultadoR1,"El resultado del número 2, es: ", resultadoR2);
 
-    document.getElementById("salida").textContent = "La raiz cuadrada de: "+ n1  + " es: " + resultado 
-    + " y de: "+ n2  + " es: " + resultado1;
+    document.getElementById("salida").textContent = "La raiz cuadrada de: "+ n1  + " es: " + resultadoR1
+    + " y la raiz cuadrada de: "+ n2  + " es: " + resultadoR2;
 
     console.log("!Felicidades¡... La raiz cuadrada finalizó exitosamente")
 
