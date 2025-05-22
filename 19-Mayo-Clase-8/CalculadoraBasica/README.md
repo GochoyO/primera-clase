917c119464ef1970690b7d0b5dfbd1e66c59305e
hgonzalomarquez <hgonzalomarquez@gmail.com>
2025-05-22 16:49:01 +0000

    se arreglo falla en la cauladora y seajusto el estilo

---
+++ 19-Mayo-Clase-8/CalculadoraBasica/README.md
@@ -0,0 +1,12 @@
+-- -------------------------------------------- --
+-- 16-May-Clase-7                               --
+-- README.md                                    --
+-- -------------------------------------------- --
+
+Practica:
+
+- Tenemos dosn input enel tablero para realizar operaciones basicas de suma, resta, multiplicacion y division.
+    - Se crea el archivo css para darle estilo a la pagina.
+    - Se crea el archivo js para realizar las operaciones basicas.
+    - Se crea el archivo html para darle estructura a la pagina.
+
---
+++ 19-Mayo-Clase-8/CalculadoraBasica/calc.js
@@ -0,0 +1,109 @@
+
+const botonAll = document.querySelectorAll(".bot-1");
+
+let operacion = "";
+
+botonAll.forEach(button=> {
+    button.addEventListener("click", () => {
+
+
+    operacion = document.getElementById(botonAll).textContent;
+
+    console.log("Se hizo clic en el botón:", document.getElementById(botonAll).textContent);
+
+    if(operacion = "Suma"){
+        suma;
+    }else if(operacion = "Resta"){
+        resta;
+    }else if(operacion = "Multiplicaciòn"){
+        multiplica;
+    }else if(operacion = "División"){
+        divide;
+         }else{
+        raiz;   
+         };
+    });
+});
+
+function suma(){
+    let n1 = Number(document.getElementsByName("Numero 1")[0].value);
+    let n2 = Number(document.getElementsByName("Numero 2")[0].value);
+
+    console.log("El numero 1 es", n1);
+    console.log("El numero 2 es", n2);
+
+    let resultado = n1 + n2;
+    
+    console.log("El resultado es", resultado);
+
+    document.getElementById("salida").textContent = "Resultado de la suma es: " + resultado;
+    console.log("!Felicidades¡... La suma finalizó exitosamente")
+
+
+}
+
+const resta = () => {
+
+    let n1 = Number(document.getElementsByName("Numero 1")[0].value);
+    let n2 = Number(document.getElementsByName("Numero 2")[0].value);
+    
+    console.log("El numero 1 es", n1);
+    console.log("El numero 2 es", n2);
+
+    let resultado = n1 - n2;
+    console.log("El resultado es", resultado);
+
+    document.getElementById("salida").textContent = "Resultado de la resta es: " + resultado;
+    console.log("!Felicidades¡... La resta finalizó exitosamente")
+}
+//
+//
+function multiplica(){
+    let n1 = Number(document.getElementsByName("Numero 1")[0].value);
+    let n2 = Number(document.getElementsByName("Numero 2")[0].value);
+
+    console.log("El numero 1 es", n1);
+    console.log("El numero 2 es", n2);
+
+    let resultado = n1 * n2;
+    
+    console.log("El resultado es", resultado);
+
+    document.getElementById("salida").textContent = "Resultado de la multiplicación es: " + resultado;
+    console.log("!Felicidades¡... La multiplicación finalizó exitosamente")
+}
+
+const divide = () => {
+
+    let n1 = Number(document.getElementsByName("Numero 1")[0].value);
+    let n2 = Number(document.getElementsByName("Numero 2")[0].value);
+    
+    console.log("El numero 1 es", n1);
+    console.log("El numero 2 es", n2);
+
+    let resultado = n1 / n2;
+    console.log("El resultado es", resultado);
+
+    document.getElementById("salida").textContent = "Resultado de la división es: " + resultado;
+    console.log("!Felicidades¡... La división finalizó exitosamente")
+}
+
+function raiz(){
+    let n1 = Number(document.getElementsByName("Numero 1")[0].value);
+    let n2 = Number(document.getElementsByName("Numero 2")[0].value);
+
+    console.log("El numero 1 es", n1);
+    console.log("El numero 2 es", n2);
+
+    let resultado = Math.sqrt(n1);
+    let resultado1 = Math.sqrt(n2);
+    
+    
+    console.log("El resultado es", resultado);
+
+    document.getElementById("salida").textContent = "La raiz cuadrada de: "+ n1  + " es: " + resultado 
+    + " y de: "+ n2  + " es: " + resultado1;
+
+    console.log("!Felicidades¡... La raiz cuadrada finalizó exitosamente")
+
+}
